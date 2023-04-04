a9e280aceea832d4434d04dd685dd75ad5d96c68
daithehh04 <daithehh04@gmail.com>
2023-04-04 15:58:27 +0700

    fix time discount

--- src/UI/Clock.js
+++ src/UI/Clock.js
@@ -8,7 +8,7 @@ const Clock = () => {
 
     let interval;
     const countDown = () => {
-        const destination = new Date('April 1, 2023').getTime()
+        const destination = new Date('June 1, 2023').getTime()
 
         interval = setInterval(() => {
             const now = new Date().getTime()
